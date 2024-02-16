4092d3624d69690568993d150c56366562ca8696
sina-byn <sina.sina.bayan@protonmail.com>
2024-02-16 14:34:15 +0330

    SubmitButton component updated

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -1,28 +1,52 @@
 'use client';
 
+import { useState } from 'react';
+
 // * hooks
 import { useHtmlContext } from '@/context/HtmlContextProvider';
 
+// * components
+import Modal from '@/components/Modal';
+
 const SubmitButton = () => {
+  const [similarityPercent, setSimilarityPercent] = useState<number>();
+  const [loading, setLoading] = useState<boolean>(false);
   const { html } = useHtmlContext();
 
   const clickHandler = async () => {
-    const res = await fetch('/api/challenge?id=1', {
-      method: 'POST',
-      body: JSON.stringify({ html }),
-    });
+    setLoading(true);
 
-    const data = await res.json();
-    console.log(data);
+    try {
+      const res = await fetch('/api/challenge?id=1', {
+        method: 'POST',
+        body: JSON.stringify({ html }),
+      });
+
+      const { data } = await res.json();
+      setSimilarityPercent(data.similarity);
+    } catch (err) {
+      console.error(err);
+    }
+
+    setLoading(false);
   };
 
   return (
     <button
       type='button'
+      disabled={loading}
       onClick={clickHandler}
-      className='w-full bg-orange-500 hover:bg-orange-600 rounded-md p-2 mt-10'
+      className='w-full bg-orange-500 hover:bg-orange-600 disabled:bg-orange-600/50 rounded-md p-2 mt-10'
     >
       Submit
+      <Modal isOpen={similarityPercent !== undefined}>
+        <div className='text-xl text-orange-500 font-bold'>Congrats !!!</div>
+        <p className='mt-3'>
+          You finished the challenge with
+          <span className='text-orange-500 font-bold mx-1'>{similarityPercent}%</span>
+          similarity. 😊
+        </p>
+      </Modal>
     </button>
   );
 };
