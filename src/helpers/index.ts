297cfc42dc29f12c1784cc70a470d8829fafe259
sina-byn <sina.sina.bayan@protonmail.com>
2024-02-16 13:56:01 +0330

    readPixels util added

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,11 @@
+import { promisify } from 'util';
+import getPixels from 'get-pixels';
+
+const readPixels = async (imagePath: string) => {
+  const res = await promisify(getPixels)(imagePath, 'image/png');
+  return res.data;
+};
+
 const meanSquaredError = (a: Uint8Array, b: Uint8Array) => {
   const maxPossibleError = 255 ** 2;
   let error = 0;
@@ -12,4 +20,4 @@ const meanSquaredError = (a: Uint8Array, b: Uint8Array) => {
   return parseFloat(errorPercent.toFixed(2));
 };
 
-export { meanSquaredError };
+export { meanSquaredError, readPixels };
